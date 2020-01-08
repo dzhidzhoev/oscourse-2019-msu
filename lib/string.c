c5d2cbde46af1a146789f5228063e86592869c5f
Vladislav Dzhidzhoev <dzhidzhoev@gmail.com>
2020-01-08 11:06:53 +0300

    Added strncat

--- lib/string.c
+++ lib/string.c
@@ -62,6 +62,18 @@ strncpy(char *dst, const char *src, size_t size) {
 	return ret;
 }
 
+char *
+strncat (char *s1, const char *s2, size_t n)
+{
+  char *s = s1;
+  /* Find the end of S1.  */
+  s1 += strlen (s1);
+  size_t ss = strnlen (s2, n);
+  s1[ss] = '\0';
+  memcpy (s1, s2, ss);
+  return s;
+}
+
 size_t
 strlcpy(char *dst, const char *src, size_t size)
 {
